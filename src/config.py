25873424e8c9f96a27a36ba3f0d0e70b36e11ffc
Sean Thawe <sean.thawe@outlook.com>
2025-01-25 03:17:02 +0200

    chore: externalized model name, provider and data to be imported into config

--- src/config.py
+++ src/config.py
@@ -2,5 +2,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-MODEL_NAME = "groq/llama-3.1-8b-instant"
+MODEL_NAME = os.getenv('MODEL_NAME')
+PROVIDER = os.getenv('PROVIDER')
+DATASET = os.getenv('DATASET')
